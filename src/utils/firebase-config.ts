3bc87a01f6d5ecdb507b160a66d504aed8a23595
tunahanbucak <tunahanbucak1@gmail.com>
2023-11-13 09:33:08 +0300

    firebase updated

--- src/utils/firebase-config.ts
+++ src/utils/firebase-config.ts
@@ -1,16 +1,18 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
+import { getAnalytics } from "firebase/analytics";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBm1bQDEHl26XWfjPaJJgo0PmqnYCVsTd0",
-  authDomain: "netflix-8ba4d.firebaseapp.com",
-  projectId: "netflix-8ba4d",
-  storageBucket: "netflix-8ba4d.appspot.com",
-  messagingSenderId: "5695517899",
-  appId: "1:5695517899:web:b13e558ad76396ac78d74e",
-  measurementId: "G-Q9YJS2M99K",
+  apiKey: "AIzaSyDDOjwLOFTzXKYyVFmw5FEWu6sylc96EG0",
+  authDomain: "netflix-clone-43d1d.firebaseapp.com",
+  projectId: "netflix-clone-43d1d",
+  storageBucket: "netflix-clone-43d1d.appspot.com",
+  messagingSenderId: "1039332578426",
+  appId: "1:1039332578426:web:5275f47e720fd8a9aa97c3",
+  measurementId: "G-J1NCWM7PGM",
 };
 
 const app = initializeApp(firebaseConfig);
-
+const analytics = getAnalytics(app);
 export const firebaseAuth = getAuth(app);
